0e316642a9f41e86c2a36e45e68e23c3e6c6e099
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-08-23 16:29:50 +0200

    pseudo rendu unique

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -27,7 +27,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(length: 50, unique: true)]
     private ?string $pseudo = null;
 
     #[ORM\Column(length: 255, nullable: true)]
