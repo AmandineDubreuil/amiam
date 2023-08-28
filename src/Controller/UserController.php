8e02af356a7681e90c7cf1509554c828f5ace852
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-08-28 16:18:47 +0200

    route user définie pour user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,9 +13,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 #[Route('/user')]
 class UserController extends AbstractController
