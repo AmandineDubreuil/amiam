309a4e587dfd8540390daa58b54ac48151b33b43
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-08-29 12:23:44 +0200

    mise en forme register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -45,7 +45,7 @@ class RegistrationFormType extends AbstractType
            
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
-                'label' => 'Je confirme avoir lu et accepter les #DOCUMENTATION# du site Amiam.',
+                'label' => ' ',
                 'label_html' => true,
                 'constraints' => [
                     new IsTrue(),
