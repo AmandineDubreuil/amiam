83779e660a1d8bbc46d374ba671ba7986146d324
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-09-11 11:03:46 +0200

    ckeditor reprend les champs pour modification

--- public/assets/js/ckeditor.js
+++ public/assets/js/ckeditor.js
@@ -1,39 +1,73 @@
 // initialise ckeditor (en fonction version https://ckeditor.com/ckeditor-5/download/?null-addons=)
-console.log("ckeditor.js");
-console.dir(document.querySelector(".recetteNewEdit form button"));
 
+$description = document.querySelector("#recette_description").defaultValue;
+console.dir($description);
 
-
-InlineEditor
-.create( document.querySelector( '#editor' ), {
-
+InlineEditor.create(document.querySelector("#editor"), {
   // toolbar
-  toolbar: [ 'undo', 'redo', 
-  '|', 'heading',
-  '|', 'bold', 'italic', 'subscript',
-  '|', 'link', 'blockQuote',
-  '|', 'bulletedList', 'numberedList', 'outdent', 'indent' ],
+  toolbar: [
+    "undo",
+    "redo",
+    "|",
+    "heading",
+    "|",
+    "bold",
+    "italic",
+    "subscript",
+    "|",
+    "link",
+    "blockQuote",
+    "|",
+    "bulletedList",
+    "numberedList",
+    "outdent",
+    "indent",
+  ],
   shouldNotGroupWhenFull: true,
 
   //heading
   heading: {
     options: [
-        { model: 'paragraph', title: 'Paragraphe', class: 'ck-heading_paragraph' },
-        { model: 'heading1', view: 'h1', title: 'Titre 1', class: 'ck-heading_heading1' },
-        { model: 'heading2', view: 'h2', title: 'Titre 2', class: 'ck-heading_heading2' },
-        { model: 'heading3', view: 'h3', title: 'Titre 3', class: 'ck-heading_heading3' }
+      {
+        model: "paragraph",
+        title: "Paragraphe",
+        class: "ck-heading_paragraph",
+      },
+      {
+        model: "heading1",
+        view: "h1",
+        title: "Titre 1",
+        class: "ck-heading_heading1",
+      },
+      {
+        model: "heading2",
+        view: "h2",
+        title: "Titre 2",
+        class: "ck-heading_heading2",
+      },
+      {
+        model: "heading3",
+        view: "h3",
+        title: "Titre 3",
+        class: "ck-heading_heading3",
+      },
+    ],
+  },
+})
+ 
+  .catch((error) => {
+    console.error(error);
+  })
+  .then((editor) => {
+     // intégration texte déjà présent
+    // editor = newEditor;
+    editor.setData($description);
 
-    ]
-}
-} )
-.catch( error => {
-    console.error( error );
-} ).then((editor) => {
     document
       .querySelector(".recetteNewEdit form")
       .addEventListener("submit", function (e) {
         e.preventDefault();
-        this.querySelector("#recette_description").value = editor.getData(); 
+        this.querySelector("#recette_description").value = editor.getData();
         this.submit();
       });
   });
