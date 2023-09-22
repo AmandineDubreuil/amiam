2b58bbda6c12ccf53833da51f986888a332d16ad
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-09-22 13:22:50 +0200

    mise en forme repas/new et fin d enregistrement d un repas

--- src/Repository/RepasRepository.php
+++ src/Repository/RepasRepository.php
@@ -45,4 +45,15 @@ class RepasRepository extends ServiceEntityRepository
 //            ->getOneOrNullResult()
 //        ;
 //    }
+
+public function findByUser($value): array
+{
+    return $this->createQueryBuilder('r')
+        ->andWhere('r.user = :val')
+        ->setParameter('val', $value)
+        ->orderBy('r.id', 'ASC')
+        ->getQuery()
+        ->getResult();
+}
+
 }
