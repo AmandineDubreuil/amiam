3303ef95b8f6924247996ad7396af90bda2bd4d4
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-09-25 12:42:43 +0200

    modification recette dans repas meme regle filtre que creation repas

--- src/Controller/RepasController.php
+++ src/Controller/RepasController.php
@@ -9,6 +9,7 @@ use App\Form\RepasCommentType;
 use App\Repository\AmiRepository;
 use App\Repository\RepasRepository;
 use App\Repository\RecetteRepository;
+use App\Repository\AllergeneRepository;
 use App\Repository\AmiFamilleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
@@ -157,28 +158,183 @@ class RepasController extends AbstractController
         Repas $repa,
         EntityManagerInterface $entityManager,
         RecetteRepository $recetteRepository,
+        AllergeneRepository $allergeneRepository,
     ): Response {
+        $user = $this->security->getUser();
+
         $repasId = $repa->getId();
+        $amis = $repa->getAmis();
+        $recettes = $recetteRepository->findBy(['user' => $user]);
+        $regimes = "";
+        $allergiesGroupe = "";
+        $allergiesAliment = "";
+        $degouts = "";
+        $regimeSsPorc = 0;
+        $recettePorc = 0;
+        $recetteAEviter = 0;
+        $recettesOk = [];
+
+        foreach ($amis as $ami) {
+            //récupérer leurs régimes
+            $regimes = $ami->getRegimes();
+            foreach ($regimes as $regime) {
+                $regimesPresents[] = $regime;
+
+                // récupération spécifique régimes sans porc pour filtre des recettes
+                if ($regime->getRegime() === 'Halal') {
+                    $regimeSsPorc += 1;
+                }
+                if ($regime->getRegime() === 'Sans porc') {
+                    $regimeSsPorc += 1;
+                }
+            }
+
+            // récupérer leurs allergies groupe pour filtre des recettes
+            $allergiesGroupe = $ami->getAllergies();
+            foreach ($allergiesGroupe as $allergieGroupe) {
+                $allergiesGroupePresents[] = $allergieGroupe;
+            }
+
+            // récupérer leurs allergies aliment pour filtre des recettes
+            $allergiesAliment = $ami->getAllergiesAliment();
+            foreach ($allergiesAliment as $al) {
+                $allergiesAlimentPresentes[] = $al;
+            }
+
+            //  récupérer leurs dégouts pour filtre des recettes
+            $degouts = $ami->getDegout();
+            foreach ($degouts as $degout) {
+                $degoutsPresents[] = $degout;
+            }
+
+            ############  fin de la boucle amiPresent
+        }
+
+
+        // créer tableau vide si pas d'allergies Aliment, pas de dégout, pas d'allergie groupe
+        if (empty($allergiesAlimentPresentes)) {
+            $allergiesAlimentPresentes = [];
+        }
+        if (empty($degoutsPresents)) {
+            $degoutsPresents = [];
+        }
+        if (empty($allergiesGroupePresents)) {
+            $allergiesGroupePresents = [];
+        }
+
+        ########## DEBUT PARTIE RECETTES #########
+
+        //récupérer les recettes avec allergies, dégouts dans un tableau
+        // récupération également des recettes contenant du porc si une personne au moins souhaite sans porc
+
+        foreach ($recettes as $recette) {
+
+            $ingredients = $recette->getIngredients();
+            //   récupération des ingrédients de la recette
+            foreach ($ingredients as $ingredient) {
+
+                // récupération ingrédient
+                $aliment = $ingredient->getAliment();
+                $alimentArray[] = $aliment;
+
+                //récupération allergène
+                $allergene = $aliment->getAllergene();
+                $allergene1stArray = $allergene->toArray();
+                $allergeneString = implode(',', $allergene1stArray);
+                $allergeneArray[] = $allergeneRepository->find($allergeneString);
+
+                //récupération aliment à base de porc
+                // $recettePorc = 0;
+                $sousGroupeAliment = $aliment->getSousGroupe();
+                $sousGroupeString = $sousGroupeAliment->getSousGroupe();
+
+                if ($sousGroupeString === 'Porc') {
+                    $recettePorc += 1;
+                }
+            }
+
+
+            ######## pour AllergiesAliment #########
+            // comparer les ingrédients de la recette aux allergies présentes
+
+            $recettesAvecAllergieAlimentConstruct = array_intersect($alimentArray, $allergiesAlimentPresentes);
+
+            ######## pour Degouts #########
+            // comparer les ingrédients de la recette aux dégouts présents
+
+            $recettesAvecDegoutConstruct = array_intersect($alimentArray, $degoutsPresents);
+
+            ######## pour AllergiesGroupe #########
+            // comparer les allergenes de la recette aux allergiesGroupe présents
+            $recettesAvecAllergeneConstruct = array_intersect($allergeneArray, $allergiesGroupePresents);
+
+            foreach ($ingredients as $ingredient) {
+                $aliment = $ingredient->getAliment();
+
+                ######## pour AllergiesAliment #########
+                if (!empty($recettesAvecAllergieAlimentConstruct) && in_array($aliment, $recettesAvecAllergieAlimentConstruct)) {
+                    // ajouter la recette au tableau $recettesConformes
+                    // $recettesAvecAllergieAliment[] = $recette;
+                    $recetteAEviter += 1;
+                }
+
+                ######## pour Degouts #########
+                if (!empty($recettesAvecDegoutConstruct) && in_array($aliment, $recettesAvecDegoutConstruct)) {
+                    // ajouter la recette au tableau $recettesConformes
+                    //   $recettesAvecDegout[] = $recette;
+                    $recetteAEviter += 1;
+                }
+
+                ######## pour AllergiesGroupe #########
+                $allergene = $aliment->getAllergene();
+                $allergene1stArray = $allergene->toArray();
+                $allergeneString = implode(',', $allergene1stArray);
+                $allergeneEntity = $allergeneRepository->find($allergeneString);
+
+                if (!empty($recettesAvecAllergeneConstruct) && in_array($allergeneEntity, $recettesAvecAllergeneConstruct)) {
+                    // ajouter la recette au tableau $recettesConformes
+                    //  $recettesAvecAllergene[] = $recette;
+                    $recetteAEviter += 1;
+                }
+            }
+
+            ######## pour régimes sans porc et halal #########
+            if ($regimeSsPorc > 0) {
+
+                if ($recettePorc > 0) {
+                    $recettePorc = 0;
+                    $recetteAEviter += 1;
+                }
+            }
+
+            ######## RECUPERATION DES RECETTES OK #########
+            if ($recetteAEviter === 0) {
+                $recettesOk[] = $recette;
+            } else {
+                $recetteAEviter = 0;
+            }
+
+            ############  fin de la boucle recette
+        }
 
         if ($request->isMethod('POST') && $request->request->has('submit')) {
             $recetteId = $request->request->get('recetteId');
             $recette = $recetteRepository->find($recetteId);
-       // dd($recette);
-   
+            // dd($recette);
+
             $repa->setRecettes($recette);
             $entityManager->persist($repa);
             $entityManager->flush();
 
-            
+
             return $this->redirectToRoute('app_repas_show', [
                 'id' => $repasId,
 
             ], Response::HTTP_SEE_OTHER);
-            
         }
         return $this->render('repas/edit_recette.html.twig', [
             'repa' => $repa,
-            //  'recettes' => $recettes,
+            'recettesOk' => $recettesOk,
 
         ]);
     }
