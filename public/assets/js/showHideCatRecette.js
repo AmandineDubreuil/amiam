aca1ab4687562ca41c3f57c93853bf7537eca893
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-09-26 11:27:19 +0200

    afficher masquer catégorie de recette dans newRepas

--- public/assets/js/showHideCatRecette.js
+++ public/assets/js/showHideCatRecette.js
@@ -9,11 +9,24 @@ const plat = document.querySelector("#plat");
 
 const dessertH2 = document.querySelector("#dessertH2");
 const dessert = document.querySelector("#dessert");
+//
+const aperitifH2AmisPresents = document.querySelector("#aperitifH2AmisPresents");
+const aperitifAmisPresents = document.querySelector("#aperitifAmisPresents");
+
+const entreeH2AmisPresents = document.querySelector("#entreeH2AmisPresents");
+const entreeAmisPresents = document.querySelector("#entreeAmisPresents");
+
+const platH2AmisPresents = document.querySelector("#platH2AmisPresents");
+const platAmisPresents = document.querySelector("#platAmisPresents");
+
+const dessertH2AmisPresents = document.querySelector("#dessertH2AmisPresents");
+const dessertAmisPresents = document.querySelector("#dessertAmisPresents");
 
 
 function showHide(divId) {
   if (divId.className === "dnone") {
     divId.className = "dblock";
+    
   } else {
     divId.className = "dnone";
   }
