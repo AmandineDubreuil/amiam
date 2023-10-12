fdc5ed3f4e4294a5e8d125ea0f89a5afe5dd55be
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-10-12 16:14:37 +0200

    ajout cgu

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -57,7 +57,7 @@ class RegistrationFormType extends AbstractType
 
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
-                'label' => 'Je confirme avoir lu et accepter les #DOCUMENTATION# du site Amiam.
+                'label' => 'Je reconnais avoir lu et compris les #DOCUMENTATION# et je les accepte.
             * ',
                 'label_html' => true,
                 'constraints' => [
